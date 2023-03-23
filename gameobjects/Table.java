85b6f0e74a5df68d1201f0e040261aac553889ef
Can Kütükoğlu <cankutukoglu03@gmail.com>
2023-03-23 20:28:19 +0300

    Some comments and minor changes in table

--- gameobjects/Table.java
+++ gameobjects/Table.java
@@ -5,4 +5,8 @@ package gameobjects;
 public class Table {
     Ball[] ballsInPlay;
     Cushion[] cushions;
+    // 1 cm = 3 pixels
+    // A classic 8-ball pool table is 112 cm x 224 cm (inner rectangle)
+    final public static int TABLE_WIDTH=336;
+    final public static int TABLE_HEIGHT=672;
 }
--- temporaryUIPanel/Main.java
+++ temporaryUIPanel/Main.java
@@ -3,8 +3,8 @@ package temporaryUIPanel;
 public class Main {
     // 142 cm=426 pixels
     // 1 meter=300 pixels
-    final public static int TABLE_WIDTH=426;
-    final public static int TABLE_HEIGHT=872;
+    final public static int TABLE_WIDTH=336;
+    final public static int TABLE_HEIGHT=672;
     public static void main(String[] args) {
         new BilliardsFrame();
     }
