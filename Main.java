180a41dc24b551cc10bb290838b5d703c7082837
Umut Utku ERŞAHİNCE <umut-er@Umuts-MacBook-Pro.local>
2023-04-27 11:31:30 +0300

    ui main deleted, we now have global main

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
-import vectormath.*;
+import ui.PoolFrame;
 
 // TODO: Figure out a project structure (packages and classes).
 
 public class Main {
     public static void main(String[] args){
-        double[] data = {5, 12, 0};
-        Vector3 vector = new Vector3(data);
-        System.out.println(vector.getVectorLength());
+        PoolFrame frame = new PoolFrame();
+        frame.start();
     }
 }
--- ui/Main.java
+++
@@ -1,8 +0,0 @@
-package ui;
-
-public class Main {
-    public static void main(String[] args) {
-        PoolFrame frame = new PoolFrame();
-        frame.start();
-    }
-}
