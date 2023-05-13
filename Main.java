e4aad05d2b0ee7513b4e8fa627edc6a37a5ac23a
Can Kütükoğlu <cankutukoglu03@gmail.com>
2023-05-13 16:25:04 +0300

    MenuFrame is done for now, maybe we can add some images etc. later

--- Main.java
+++ Main.java
@@ -2,12 +2,14 @@ import java.io.UnsupportedEncodingException;
 import database.PoolDatabase;
 import net.thegreshams.firebase4j.error.FirebaseException;
 import net.thegreshams.firebase4j.error.JacksonUtilityException;
+import ui.MenuFrame;
 import ui.PoolFrame;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException, JacksonUtilityException, FirebaseException{
         PoolDatabase database=new PoolDatabase();
-        PoolFrame frame = new PoolFrame("User1", "User2");
-        frame.start();
+        MenuFrame menu=new MenuFrame();
+        //PoolFrame frame = new PoolFrame("User1", "User2");
+        //frame.start();
     }
 }
