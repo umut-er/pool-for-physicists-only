a556d1b27d0258b8025188a93e924e7356103686
Can Kütükoğlu <cankutukoglu03@gmail.com>
2023-05-14 00:06:36 +0300

    minor change

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 import java.io.UnsupportedEncodingException;
-import database.PoolDatabase;
 import net.thegreshams.firebase4j.error.FirebaseException;
 import net.thegreshams.firebase4j.error.JacksonUtilityException;
 import ui.MenuFrame;
-import ui.PoolFrame;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException, JacksonUtilityException, FirebaseException{
         MenuFrame menu=new MenuFrame();
+        // Existing accounts
+        // User1 / user1234
+        // User2 / player1234
     }
 }
