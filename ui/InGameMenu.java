9f3f4ec9d6a56067928156b95935c3240b66aa5b
Can Kütükoğlu <cankutukoglu03@gmail.com>
2023-05-28 11:41:39 +0300

    minor changes

--- ui/InGameMenu.java
+++ ui/InGameMenu.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -18,7 +20,7 @@ public class InGameMenu extends JPanel implements ActionListener{
         this.panel=panel;
         Font font=new Font("Dialog", Font.PLAIN, 16);
         this.resumeButton=new JButton("Resume");
-        this.exitButton=new JButton("Exit");
+        this.exitButton=new JButton("Menu");
         this.setBounds(1000, 375, 100, 100);
         this.setVisible(false);
         this.setLayout(new GridLayout(2,1));
@@ -37,9 +39,9 @@ public class InGameMenu extends JPanel implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==this.exitButton)
+        if(e.getSource()==exitButton)
         {
-            //TODO Exit Fix
+            //TODO Return to menu fix
         }
         else
         {
